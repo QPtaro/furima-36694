b7572defaf6bd3fe273cbf3309b49c62593d48e5
QPtaro <m.kyutaro56@gmail.com>
2022-01-11 00:58:52 +0900

    itemモデル　作成

---
+++ app/models/item.rb
@@ -0,0 +1,19 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :delivery_time
+
+  validates :item_name, :item_description, :item_price, presence: true
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :delivery_fee_id, numericality: { other_than: 1 }
+  validates :delivery_time_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+end
+### Association
+# belongs_to :user
+# has_one :purchase
