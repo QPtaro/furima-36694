abb99988075be21eb606975b121fde15e20d8e4c
QPtaro <m.kyutaro56@gmail.com>
2022-01-16 23:18:40 +0900

    javascript実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function(){
+
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTax = this.document.getElementById("add-tax-price");
+    addTax.innerHTML = (Math.floor(inputValue * 0.1));
+     const profitNumber = document.getElementById("profit")
+     const value_result = inputValue * 0.1
+     profitNumber.innerHTML = (Math.floor(inputValue - value_result));
+  })
+})
