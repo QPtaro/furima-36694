c763818648f694c46ea2bbaf5ec8e9467250845c
QPtaro <m.kyutaro56@gmail.com>
2022-01-16 20:06:10 +0900

    保存機能　コントローラー実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,14 @@ class ItemsController < ApplicationController
    @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :item_name, :item_description, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :delivery_time_id, :item_price, :user_id)
+  end
+
 end
