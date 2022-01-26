904fac2a72565dd9716f975f2a6b301ade37bdb6
QPtaro <m.kyutaro56@gmail.com>
2022-01-26 16:42:36 +0900

    itemモデルの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,15 +6,16 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :delivery_time
   has_one_attached :image
+  belongs_to :user
+  has_one :purchase
 
-  validates :item_name, :item_description, :item_price, presence: true
-
+  validates :image, :item_name, :item_description, :item_price, presence: true
   validates :category_id, numericality: { other_than: 1 }
   validates :condition_id, numericality: { other_than: 1 }
   validates :delivery_fee_id, numericality: { other_than: 1 }
   validates :delivery_time_id, numericality: { other_than: 1 }
   validates :prefecture_id, numericality: { other_than: 1 }
+  validates :item_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+  format: { with: /\A[0-9]+\z/ }
+
 end
-### Association
-# belongs_to :user
-# has_one :purchase
