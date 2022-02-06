d003335eb573162b15cd6eac419400b750939b2a
QPtaro <m.kyutaro56@gmail.com>
2022-02-06 18:33:27 +0900

    ファクトリーボット修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     item_name { '名前' }
     item_description { '説明' }
-    category_id { '2' }
-    condition_id { '2' }
-    delivery_fee_id { '2' }
-    prefecture_id { '2' }
-    delivery_time_id { '2' }
-    item_price { '1000' }
+    category_id { 2 }
+    condition_id { 2 }
+    delivery_fee_id { 2 }
+    prefecture_id { 2 }
+    delivery_time_id { 2 }
+    item_price { 1000 }
     association :user
 
     after(:build) do |message|
