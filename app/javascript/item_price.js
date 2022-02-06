f77687a8c49fa7a3b793a2ff47368cb6c954bee3
QPtaro <m.kyutaro56@gmail.com>
2022-02-06 18:33:52 +0900

    javascript修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -4,7 +4,7 @@ window.addEventListener('load', function(){
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
     const addTax = this.document.getElementById("add-tax-price");
-    addTax.innerHTML = (Math.floor(inputValue * 0.1));
+    addTax.innerHTML = (Math.round(inputValue * 0.1));
      const profitNumber = document.getElementById("profit")
      const value_result = inputValue * 0.1
      profitNumber.innerHTML = (Math.round(inputValue - value_result));
